292efe2057b4009f404dcbef0efbb44c91b3328b
himanshu vashistha <113257378+coderstar1234@users.noreply.github.com>
2023-02-23 05:50:43 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,50 @@
+
+// Checking if a number is a Int or float 
+
+let int = 1
+let Fl = 1.1
+
+let bar = "--------------"
+
+
+// A function 👀
+
+const check = (x) => { 
+    
+    // Checking if a Input is a nimber or not
+
+    if (typeof(x) === "number") {
+        
+        // Checking if a the number is a float value or Integer value
+
+        if (Number.isInteger(x)) {
+        // Integer
+            console.log(x+" Is a Integer");
+        }
+        
+        else {
+        // float 
+            console.log(x+" Is a Float ");
+        }
+       // Made By T-Hassaan 💖
+    }
+    // JS Is Fun ✌️😂
+}
+
+// Callimg our functions 🤤 With respect 😂
+
+console.log(bar)
+console.log("> Showing The New DataTypes")
+console.log(bar)
+check(int)
+console.log(bar)
+check(Fl)
+console.log(bar)
+console.log("\n⭐ Made By Techno Hassaan ⭐")
+console.log("\n⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐")
+console.log("\nHi, Everyone my JavaScript Journey is going to touch the sky 🥰😍")
+console.log("\n⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐")
+console.log(" So plzz 😴\n\n > If U Love It 🤍\n > Follow 😃\n > Like 👍\n > Share 💞")
+
+// Happy JavaScript 😍
+
